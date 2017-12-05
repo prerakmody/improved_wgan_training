bc62dfab29a898c0823bc298f602928ceb4ead17
strider <prerakmody@gmail.com>
2017-12-05 17:43:20 +0530

    Demo for MNIST is complete

--- demo/gan_mnist/gan_mnist.py
+++ demo/gan_mnist/gan_mnist.py
@@ -1,15 +1,11 @@
 import os, sys
-sys.path.append(os.getcwd())
-
 import time
-
 import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.datasets
-import tensorflow as tf
+import matplotlib.pyplot as plt
 
+import tensorflow as tf
 import tflib as lib
 import tflib.ops.linear
 import tflib.ops.conv2d
@@ -19,235 +15,259 @@ import tflib.save_images
 import tflib.mnist
 import tflib.plot
 
-MODE = 'wgan-gp' # dcgan, wgan, or wgan-gp
-DIM = 64 # Model dimensionality
-BATCH_SIZE = 50 # Batch size
-CRITIC_ITERS = 5 # For WGAN and WGAN-GP, number of critic iters per gen iter
-LAMBDA = 10 # Gradient penalty lambda hyperparameter
-ITERS = 200000 # How many generator iterations to train for 
-OUTPUT_DIM = 784 # Number of pixels in MNIST (28*28)
+# lib.print_model_settings(locals().copy())
 
-lib.print_model_settings(locals().copy())
+class GAN_MNIST():
+    def __init__(self, mode = 'wgan-gp', dim = 64, output_dim = 28 * 28, batch_size = 50, lambdaa = 10):  
+        """
+        mode : str
+            - dcgan, wgan, or wgan-gp
+        """
+        self.mode = mode
+        self.dim = dim
+        self.output_dim = output_dim
+        self.batch_size = batch_size
+        self.lambdaa = lambdaa
 
-def LeakyReLU(x, alpha=0.2):
-    return tf.maximum(alpha*x, x)
+        self.loss()
 
-def ReLULayer(name, n_in, n_out, inputs):
-    output = lib.ops.linear.Linear(
-        name+'.Linear', 
-        n_in, 
-        n_out, 
-        inputs,
-        initialization='he'
-    )
-    return tf.nn.relu(output)
+        self.train_gen, self.dev_gen, self.test_gen = lib.mnist.load(self.batch_size, self.batch_size)
+    
+    def LeakyReLU(self, x, alpha=0.2):
+        return tf.maximum(alpha*x, x)
 
-def LeakyReLULayer(name, n_in, n_out, inputs):
-    output = lib.ops.linear.Linear(
-        name+'.Linear', 
-        n_in, 
-        n_out, 
-        inputs,
-        initialization='he'
-    )
-    return LeakyReLU(output)
-
-def Generator(n_samples, noise=None):
-    if noise is None:
-        noise = tf.random_normal([n_samples, 128])
-
-    output = lib.ops.linear.Linear('Generator.Input', 128, 4*4*4*DIM, noise)
-    if MODE == 'wgan':
-        output = lib.ops.batchnorm.Batchnorm('Generator.BN1', [0], output)
-    output = tf.nn.relu(output)
-    output = tf.reshape(output, [-1, 4*DIM, 4, 4])
-
-    output = lib.ops.deconv2d.Deconv2D('Generator.2', 4*DIM, 2*DIM, 5, output)
-    if MODE == 'wgan':
-        output = lib.ops.batchnorm.Batchnorm('Generator.BN2', [0,2,3], output)
-    output = tf.nn.relu(output)
-
-    output = output[:,:,:7,:7]
-
-    output = lib.ops.deconv2d.Deconv2D('Generator.3', 2*DIM, DIM, 5, output)
-    if MODE == 'wgan':
-        output = lib.ops.batchnorm.Batchnorm('Generator.BN3', [0,2,3], output)
-    output = tf.nn.relu(output)
-
-    output = lib.ops.deconv2d.Deconv2D('Generator.5', DIM, 1, 5, output)
-    output = tf.nn.sigmoid(output)
-
-    return tf.reshape(output, [-1, OUTPUT_DIM])
-
-def Discriminator(inputs):
-    output = tf.reshape(inputs, [-1, 1, 28, 28])
-
-    output = lib.ops.conv2d.Conv2D('Discriminator.1',1,DIM,5,output,stride=2)
-    output = LeakyReLU(output)
-
-    output = lib.ops.conv2d.Conv2D('Discriminator.2', DIM, 2*DIM, 5, output, stride=2)
-    if MODE == 'wgan':
-        output = lib.ops.batchnorm.Batchnorm('Discriminator.BN2', [0,2,3], output)
-    output = LeakyReLU(output)
-
-    output = lib.ops.conv2d.Conv2D('Discriminator.3', 2*DIM, 4*DIM, 5, output, stride=2)
-    if MODE == 'wgan':
-        output = lib.ops.batchnorm.Batchnorm('Discriminator.BN3', [0,2,3], output)
-    output = LeakyReLU(output)
-
-    output = tf.reshape(output, [-1, 4*4*4*DIM])
-    output = lib.ops.linear.Linear('Discriminator.Output', 4*4*4*DIM, 1, output)
-
-    return tf.reshape(output, [-1])
-
-real_data = tf.placeholder(tf.float32, shape=[BATCH_SIZE, OUTPUT_DIM])
-fake_data = Generator(BATCH_SIZE)
-
-disc_real = Discriminator(real_data)
-disc_fake = Discriminator(fake_data)
-
-gen_params = lib.params_with_name('Generator')
-disc_params = lib.params_with_name('Discriminator')
-
-if MODE == 'wgan':
-    gen_cost = -tf.reduce_mean(disc_fake)
-    disc_cost = tf.reduce_mean(disc_fake) - tf.reduce_mean(disc_real)
-
-    gen_train_op = tf.train.RMSPropOptimizer(
-        learning_rate=5e-5
-    ).minimize(gen_cost, var_list=gen_params)
-    disc_train_op = tf.train.RMSPropOptimizer(
-        learning_rate=5e-5
-    ).minimize(disc_cost, var_list=disc_params)
-
-    clip_ops = []
-    for var in lib.params_with_name('Discriminator'):
-        clip_bounds = [-.01, .01]
-        clip_ops.append(
-            tf.assign(
-                var, 
-                tf.clip_by_value(var, clip_bounds[0], clip_bounds[1])
-            )
+    def ReLULayer(self, name, n_in, n_out, inputs):
+        output = lib.ops.linear.Linear(
+            name+'.Linear', 
+            n_in, 
+            n_out, 
+            inputs,
+            initialization='he'
         )
-    clip_disc_weights = tf.group(*clip_ops)
+        return tf.nn.relu(output)
 
-elif MODE == 'wgan-gp':
-    gen_cost = -tf.reduce_mean(disc_fake)
-    disc_cost = tf.reduce_mean(disc_fake) - tf.reduce_mean(disc_real)
+    def LeakyReLULayer(self, name, n_in, n_out, inputs):
+        output = lib.ops.linear.Linear(
+            name+'.Linear', 
+            n_in, 
+            n_out, 
+            inputs,
+            initialization='he'
+        )
+        return self.LeakyReLU(output)
 
-    alpha = tf.random_uniform(
-        shape=[BATCH_SIZE,1], 
-        minval=0.,
-        maxval=1.
-    )
-    differences = fake_data - real_data
-    interpolates = real_data + (alpha*differences)
-    gradients = tf.gradients(Discriminator(interpolates), [interpolates])[0]
-    slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
-    gradient_penalty = tf.reduce_mean((slopes-1.)**2)
-    disc_cost += LAMBDA*gradient_penalty
+    def Generator(self, n_samples, noise=None):
+        if noise is None:
+            noise = tf.random_normal([n_samples, 128])
 
-    gen_train_op = tf.train.AdamOptimizer(
-        learning_rate=1e-4, 
-        beta1=0.5,
-        beta2=0.9
-    ).minimize(gen_cost, var_list=gen_params)
-    disc_train_op = tf.train.AdamOptimizer(
-        learning_rate=1e-4, 
-        beta1=0.5, 
-        beta2=0.9
-    ).minimize(disc_cost, var_list=disc_params)
+        output = lib.ops.linear.Linear('Generator.Input', 128, 4 * 4 * 4 * self.dim, noise)
+        if self.mode == 'wgan':
+            output = lib.ops.batchnorm.Batchnorm('Generator.BN1', [0], output)
+        output = tf.nn.relu(output)
+        output = tf.reshape(output, [-1, 4 * self.dim, 4, 4])
 
-    clip_disc_weights = None
+        output = lib.ops.deconv2d.Deconv2D('Generator.2', 4 * self.dim, 2 * self.dim, 5, output)
+        if self.mode == 'wgan':
+            output = lib.ops.batchnorm.Batchnorm('Generator.BN2', [0,2,3], output)
+        output = tf.nn.relu(output)
 
-elif MODE == 'dcgan':
-    gen_cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-        disc_fake, 
-        tf.ones_like(disc_fake)
-    ))
+        output = output[:,:,:7,:7]
 
-    disc_cost =  tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-        disc_fake, 
-        tf.zeros_like(disc_fake)
-    ))
-    disc_cost += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-        disc_real, 
-        tf.ones_like(disc_real)
-    ))
-    disc_cost /= 2.
+        output = lib.ops.deconv2d.Deconv2D('Generator.3', 2 * self.dim, self.dim, 5, output)
+        if self.mode == 'wgan':
+            output = lib.ops.batchnorm.Batchnorm('Generator.BN3', [0,2,3], output)
+        output = tf.nn.relu(output)
 
-    gen_train_op = tf.train.AdamOptimizer(
-        learning_rate=2e-4, 
-        beta1=0.5
-    ).minimize(gen_cost, var_list=gen_params)
-    disc_train_op = tf.train.AdamOptimizer(
-        learning_rate=2e-4, 
-        beta1=0.5
-    ).minimize(disc_cost, var_list=disc_params)
+        output = lib.ops.deconv2d.Deconv2D('Generator.5', self.dim, 1, 5, output)
+        output = tf.nn.sigmoid(output)
 
-    clip_disc_weights = None
+        return tf.reshape(output, [-1, self.output_dim])
 
-# For saving samples
-fixed_noise = tf.constant(np.random.normal(size=(128, 128)).astype('float32'))
-fixed_noise_samples = Generator(128, noise=fixed_noise)
-def generate_image(frame, true_dist):
-    samples = session.run(fixed_noise_samples)
-    lib.save_images.save_images(
-        samples.reshape((128, 28, 28)), 
-        'samples_{}.png'.format(frame)
-    )
+    def Discriminator(self, inputs):
+        output = tf.reshape(inputs, [-1, 1, 28, 28])
 
-# Dataset iterator
-train_gen, dev_gen, test_gen = lib.mnist.load(BATCH_SIZE, BATCH_SIZE)
-def inf_train_gen():
-    while True:
-        for images,targets in train_gen():
-            yield images
+        output = lib.ops.conv2d.Conv2D('Discriminator.1', 1, self.dim, 5, output,stride=2)
+        output = self.LeakyReLU(output)
 
-# Train loop
-with tf.Session() as session:
+        output = lib.ops.conv2d.Conv2D('Discriminator.2', self.dim, 2 * self.dim, 5, output, stride=2)
+        if self.dim == 'wgan':
+            output = lib.ops.batchnorm.Batchnorm('Discriminator.BN2', [0,2,3], output)
+        output = self.LeakyReLU(output)
 
-    session.run(tf.initialize_all_variables())
+        output = lib.ops.conv2d.Conv2D('Discriminator.3', 2*self.dim, 4*self.dim, 5, output, stride=2)
+        if self.mode == 'wgan':
+            output = lib.ops.batchnorm.Batchnorm('Discriminator.BN3', [0,2,3], output)
+        output = self.LeakyReLU(output)
 
-    gen = inf_train_gen()
+        output = tf.reshape(output, [-1, 4*4*4*self.dim])
+        output = lib.ops.linear.Linear('Discriminator.Output', 4*4*4*self.dim, 1, output)
 
-    for iteration in xrange(ITERS):
-        start_time = time.time()
+        return tf.reshape(output, [-1])
 
-        if iteration > 0:
-            _ = session.run(gen_train_op)
+    def loss(self):
+        self.real_data = tf.placeholder(tf.float32, shape=[self.batch_size, self.output_dim])
+        self.fake_data = self.Generator(self.batch_size)
 
-        if MODE == 'dcgan':
-            disc_iters = 1
-        else:
-            disc_iters = CRITIC_ITERS
-        for i in xrange(disc_iters):
-            _data = gen.next()
-            _disc_cost, _ = session.run(
-                [disc_cost, disc_train_op],
-                feed_dict={real_data: _data}
-            )
-            if clip_disc_weights is not None:
-                _ = session.run(clip_disc_weights)
+        self.disc_real = self.Discriminator(self.real_data)
+        self.disc_fake = self.Discriminator(self.fake_data)
 
-        lib.plot.plot('train disc cost', _disc_cost)
-        lib.plot.plot('time', time.time() - start_time)
+        gen_params = lib.params_with_name('Generator')
+        disc_params = lib.params_with_name('Discriminator')
 
-        # Calculate dev loss and generate samples every 100 iters
-        if iteration % 100 == 99:
-            dev_disc_costs = []
-            for images,_ in dev_gen():
-                _dev_disc_cost = session.run(
-                    disc_cost, 
-                    feed_dict={real_data: images}
+        if self.mode == 'wgan':
+            self.gen_cost = -tf.reduce_mean(self.disc_fake)
+            self.disc_cost = tf.reduce_mean(self.disc_fake) - tf.reduce_mean(self.disc_real)
+
+            self.gen_train_op = tf.train.RMSPropOptimizer(
+                learning_rate=5e-5
+            ).minimize(self.gen_cost, var_list=gen_params)
+            self.disc_train_op = tf.train.RMSPropOptimizer(
+                learning_rate=5e-5
+            ).minimize(self.disc_cost, var_list=disc_params)
+
+            clip_ops = []
+            for var in lib.params_with_name('Discriminator'):
+                clip_bounds = [-.01, .01]
+                clip_ops.append(
+                    tf.assign(
+                        var, 
+                        tf.clip_by_value(var, clip_bounds[0], clip_bounds[1])
+                    )
                 )
-                dev_disc_costs.append(_dev_disc_cost)
-            lib.plot.plot('dev disc cost', np.mean(dev_disc_costs))
+            self.clip_disc_weights = tf.group(*clip_ops)
 
-            generate_image(iteration, _data)
+        elif self.mode == 'wgan-gp':
+            self.gen_cost = -tf.reduce_mean(self.disc_fake)
+            self.disc_cost = tf.reduce_mean(self.disc_fake) - tf.reduce_mean(self.disc_real)
 
-        # Write logs every 100 iters
-        if (iteration < 5) or (iteration % 100 == 99):
-            lib.plot.flush()
+            alpha = tf.random_uniform(
+                shape=[self.batch_size,1], 
+                minval=0.,
+                maxval=1.
+            )
+            differences = self.fake_data - self.real_data
+            interpolates = self.real_data + (alpha*differences)
+            gradients = tf.gradients(self.Discriminator(interpolates), [interpolates])[0]
+            slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
+            gradient_penalty = tf.reduce_mean((slopes-1.)**2)
+            self.disc_cost += self.lambdaa * gradient_penalty
 
-        lib.plot.tick()
+            self.gen_train_op = tf.train.AdamOptimizer(
+                learning_rate=1e-4, 
+                beta1=0.5,
+                beta2=0.9
+            ).minimize(self.gen_cost, var_list=gen_params)
+            self.disc_train_op = tf.train.AdamOptimizer(
+                learning_rate=1e-4, 
+                beta1=0.5, 
+                beta2=0.9
+            ).minimize(self.disc_cost, var_list=disc_params)
+
+            self.clip_disc_weights = None
+
+        elif MODE == 'dcgan':
+            pass
+            # gen_cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            #     disc_fake, 
+            #     tf.ones_like(disc_fake)
+            # ))
+
+            # disc_cost =  tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            #     disc_fake, 
+            #     tf.zeros_like(disc_fake)
+            # ))
+            # disc_cost += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            #     disc_real, 
+            #     tf.ones_like(disc_real)
+            # ))
+            # disc_cost /= 2.
+
+            # gen_train_op = tf.train.AdamOptimizer(
+            #     learning_rate=2e-4, 
+            #     beta1=0.5
+            # ).minimize(gen_cost, var_list=gen_params)
+            # disc_train_op = tf.train.AdamOptimizer(
+            #     learning_rate=2e-4, 
+            #     beta1=0.5
+            # ).minimize(disc_cost, var_list=disc_params)
+
+            # clip_disc_weights = None
+
+        # For saving samples
+        fixed_noise = tf.constant(np.random.normal(size=(128, 128)).astype('float32'))
+        self.fixed_noise_samples = self.Generator(128, noise=fixed_noise)
+
+    def generate_image(self, session, iteration, true_dist):
+        samples = session.run(self.fixed_noise_samples)
+
+        save_dir = os.path.join(os.getcwd(), 'gan_mnist_output')
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        file_name = os.path.join(save_dir, 'frame' + str(iteration) + '.png')
+
+        lib.save_images.save_images(
+            samples.reshape((128, 28, 28)), 
+            file_name
+        )
+
+    # Dataset iterator
+    def inf_train_gen(self):
+        while True:
+            for images,targets in self.train_gen():
+                yield images
+
+    def train(self, critic_iters = 5, iters = 10000):
+        # Train loop
+        with tf.Session() as session:
+            print ('\n ======= TRAIN =======')
+            session.run(tf.global_variables_initializer())
+            session.run(tf.local_variables_initializer())
+            print (' ---> Initialized all variables')
+
+            gen = self.inf_train_gen()
+
+            for iteration in range(iters):
+                if iteration == 0:
+                    print (' ---> Network and data initialized. Let the training begin! And may the best weights win!')
+                start_time = time.time()
+
+                # TRAIN GENERATOR
+                if iteration > 0:
+                    _ = session.run(self.gen_train_op)
+
+                # TRAIN DISCRIMINATOR
+                if self.mode == 'dcgan':
+                    disc_iters = 1
+                else:
+                    disc_iters = critic_iters
+
+                for i in range(disc_iters):
+                    _data = gen.__next__()
+                    _disc_cost, _ = session.run(
+                        [self.disc_cost, self.disc_train_op],
+                        feed_dict={self.real_data: _data}
+                    )
+                    if self.clip_disc_weights is not None:
+                        _ = session.run(self.clip_disc_weights)
+
+                # LOGGING
+                # lib.plot.plot('train disc cost', _disc_cost)
+                # lib.plot.plot('time', time.time() - start_time)
+                # Calculate dev loss and generate samples every 100 iters
+                if iteration % 10 == 0:
+                    dev_disc_costs = []
+                    for images,_ in self.dev_gen():
+                        _dev_disc_cost = session.run(
+                            self.disc_cost, 
+                            feed_dict={self.real_data: images}
+                        )
+                        dev_disc_costs.append(_dev_disc_cost)
+                    print ('Iteration:', iteration, np.mean(dev_disc_costs))
+                    # lib.plot.plot('dev disc cost', np.mean(dev_disc_costs))
+                    self.generate_image(session, iteration, _data)
+
+                # Write logs every 100 iters
+                # if (iteration < 5) or (iteration % 100 == 99):
+                #     lib.plot.flush()
+
+                # lib.plot.tick()
--- tflib/save_images.py
+++ tflib/save_images.py
@@ -6,33 +6,111 @@ import numpy as np
 import scipy.misc
 from scipy.misc import imsave
 
-def save_images(X, save_path):
-    # [0, 1] -> [0,255]
-    if isinstance(X.flatten()[0], np.floating):
-        X = (255.99*X).astype('uint8')
+def unblockshaped(arr, h, w):
+        """
+        Return an array of shape (h, w) where
+        h * w = arr.size
 
+        If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
+        then the returned array preserves the "physical" layout of the sublocks.
+        """
+        n, nrows, ncols = arr.shape
+        return (arr.reshape(h//nrows, -1, nrows, ncols)
+                   .swapaxes(1,2)
+                   .reshape(h, w))
+
+def save_images(X, save_path):
+
+    # 1. PREPROCESSING
+    if isinstance(X.flatten()[0], np.floating):
+        X = (255.99*X).astype('uint8')  # [0, 1] -> [0,255]
+
+    # 2. GET WIDTH AND HEIGHT (to convert a 3D image -> 2D image)
     n_samples = X.shape[0]
     rows = int(np.sqrt(n_samples))
     while n_samples % rows != 0:
         rows -= 1
+    nh, nw = int(rows), int(n_samples/rows)
 
-    nh, nw = rows, n_samples/rows
+    # CONVERT 3D ARRAY TO TILED 2D IMAGE (using some numpy magic)
+    h, w = X[0].shape[:2]
+    img = np.array(unblockshaped(X, h*nh, w*nw))
+    
+    # if X.ndim == 2:
+    #     X = np.reshape(X, (X.shape[0], int(np.sqrt(X.shape[1])), int(np.sqrt(X.shape[1]))))
 
-    if X.ndim == 2:
-        X = np.reshape(X, (X.shape[0], int(np.sqrt(X.shape[1])), int(np.sqrt(X.shape[1]))))
+    # if X.ndim == 4:
+    #     # BCHW -> BHWC
+    #     X = X.transpose(0,2,3,1)
+    #     h, w = X[0].shape[:2]
+    #     img = np.zeros(( int(h*nh), int(w*nw), 3))
+    
+    # elif X.ndim == 3:
+    #     h, w = X[0].shape[:2]
+    #     img = np.zeros(( int(h*nh), int(w*nw) ))
+    
+    # print ('\nX:', X.shape, ' img:', img.shape)
+    # print ('nh:', nh, 'nw:', nw, ' h:', h, ' w:', w)
 
-    if X.ndim == 4:
-        # BCHW -> BHWC
-        X = X.transpose(0,2,3,1)
-        h, w = X[0].shape[:2]
-        img = np.zeros((h*nh, w*nw, 3))
-    elif X.ndim == 3:
-        h, w = X[0].shape[:2]
-        img = np.zeros((h*nh, w*nw))
+    # for n, x in enumerate(X):
+    #     j = n / nw
+    #     i = n % nw
+    #     print ('jh:', int(j*h), ':',int(j*h)+h, ' iw:', int(i*w),':', int(i*w)+w)
+    #     img[ int(j*h) : int(j*h)+h, int(i*w) : int(i*w)+w ] = x
 
-    for n, x in enumerate(X):
-        j = n/nw
-        i = n%nw
-        img[j*h:j*h+h, i*w:i*w+w] = x
+    # for img_id, img in enumerate(X):
+    #     block_w = int(img_id / nw) #
+    #     block_h = int(img_id % nw) #
+    #     print (block_w, block_h)
+    #     # print (int(block_w*w), int(block_w*w)+w, ',', int(block_h*h), int(block_h*h)+h, img[ int(block_w*w) : int(block_w*w)+w, int(block_h*h) : int(block_h*h)+h ].shape)
+        # img[ int(block_w*w) : int(block_w*w)+w, int(block_h*h) : int(block_h*h)+h ] = img
 
+    
     imsave(save_path, img)
+
+# PROOF OF THE unblockshaped trick
+def convert_3D_image_to_2D_image(url_image, nx = 28, ny = 28, img_copies = 128):
+    def unblockshaped(arr, h, w):
+        """
+        Return an array of shape (h, w) where
+        h * w = arr.size
+
+        If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
+        then the returned array preserves the "physical" layout of the sublocks.
+        """
+        n, nrows, ncols = arr.shape
+        return (arr.reshape(h//nrows, -1, nrows, ncols)
+                   .swapaxes(1,2)
+                   .reshape(h, w))
+
+    import numpy as np
+    from scipy.misc import imread
+    from scipy.misc import imresize
+    import matplotlib.pyplot as plt
+    # %matplotlib inline
+
+    # 1. READ AND RESIZE IMAGE
+    f, axarr = plt.subplots(1,2, figsize = (10,10))
+    a = imresize(imread(url_image,flatten=True),(nx,ny)) 
+    print ('a:',a.shape)
+    axarr[0].imshow(a)
+
+    # 2. DUPLICATE IMAGE AND CREATE A 3D ARRAY
+    X = []
+    for i in range(img_copies):
+        X.append(a)
+    X = np.array(X)
+    print ('X:',X.shape)
+    
+    tileimg_rows = int(np.sqrt(img_copies))
+    while img_copies % tileimg_rows != 0:
+        tileimg_rows -= 1
+    tileimg_cols = int(img_copies / tileimg_rows)
+    
+    print ('Tile Image: (', tileimg_rows * nx, tileimg_cols * ny,')')
+    
+    # 3. NUMPY MAGIC TO CONVERT 3D ARRAY INTO 2D IMAGE (TILE-FORMAT)
+    tmp = unblockshaped(X, tileimg_rows*ny, tileimg_cols*nx)
+    axarr[1].imshow(tmp)
+
+# convert_3D_image_to_2D_image('sample_images.jpeg')
